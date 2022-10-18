3f681e9804c041c9b4db616a5b03f0aa24f43fcd
FarzXD <110385814+FarzCracking@users.noreply.github.com>
2022-10-18 15:05:40 +0700

    config.js

--- config.js
+++ config.js
@@ -16,12 +16,12 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['62895604670507']
-global.ownernomer = "62895604670507"
-global.premium = ['62895604670507','6281328144300']
-global.packname = 'Zenss'
-global.author = 'Cuyy'
-global.sessionName = 'zenss'
+global.owner = ['6283899216451']
+global.ownernomer = "6283899216451"
+global.premium = ['6283899216451','6281328144300']
+global.packname = 'IKFAR'
+global.author = 'IFC'
+global.sessionName = 'XIAO'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
 global.mess = {
